8c5c3085f3da9e1ff7cbd9b47391aefa1e3bd96a
Cihan Korkmaz <cihankorkmaz@windowslive.com>
2022-10-26 16:20:36 +0300

    Start

---
+++ script.js
@@ -0,0 +1,101 @@
+const column1 = document.createElement("div");
+document.body.appendChild(column1);
+column1.classList.add("column")
+
+const row1 = document.createElement("div");
+column1.appendChild(row1);
+row1.classList.add("row");
+const row2 = document.createElement("div");
+column1.appendChild(row2);
+row2.classList.add("row");
+const row3 = document.createElement("div");
+column1.appendChild(row3);
+row3.classList.add("row");
+const row4 = document.createElement("div");
+column1.appendChild(row4);
+row4.classList.add("row");
+const row5 = document.createElement("div");
+column1.appendChild(row5);
+row5.classList.add("row");
+const row6 = document.createElement("div");
+column1.appendChild(row6);
+row6.classList.add("row");
+const row7 = document.createElement("div");
+column1.appendChild(row7);
+row7.classList.add("row");
+const row8 = document.createElement("div");
+column1.appendChild(row8);
+row8.classList.add("row");
+const row9 = document.createElement("div");
+column1.appendChild(row9);
+row9.classList.add("row");
+const row10 = document.createElement("div");
+column1.appendChild(row10);
+row10.classList.add("row");
+const row11 = document.createElement("div");
+column1.appendChild(row11);
+row11.classList.add("row");
+const row12 = document.createElement("div");
+column1.appendChild(row12);
+row12.classList.add("row");
+const row13 = document.createElement("div");
+column1.appendChild(row13);
+row13.classList.add("row");
+const row14 = document.createElement("div");
+column1.appendChild(row14);
+row14.classList.add("row");
+const row15 = document.createElement("div");
+column1.appendChild(row15);
+row15.classList.add("row");
+const row16 = document.createElement("div");
+column1.appendChild(row16);
+row16.classList.add("row");
+
+row1.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row2.addEventListener("mouseover", () => {
+    row2style.backgroundColor = "blue";
+});
+row3.addEventListener("mouseover", () => {
+    row3.style.backgroundColor = "blue";
+});
+row4.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row5.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row6.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row7.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row8.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row9.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row10.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row11.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row12.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row13.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row14.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row15.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
+row16.addEventListener("mouseover", () => {
+    row1.style.backgroundColor = "blue";
+});
